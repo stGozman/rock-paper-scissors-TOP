81762e43890d0006d9acfa9f49e9f5e4ad0735e4
Gozman <thegzmn@gmail.com>
2022-08-07 01:47:58 +0100

    Configure theme in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        'myBlack': '#000000',
+        'darkBlue': '#14213d',
+        'mutedYellow': '#fdff74',
+        'lightGray': '#e5e5e5',
+        'myWhite': '#ffffff',
+      },
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+        boxing: ['Boxing', 'sans-serif'],
+        sentient: ['Sentient', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
